cdbe641391425be3cfbfd4f2a88a256ef6b42800
Shashwat Sharma <shashwat2910@gmail.com>
2021-03-12 11:56:51 +0530

    Find whether an array is subset of another array

---
+++ Arrays/subsetofanotherarray.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+import Hashing.Hashset;
+
+// Find whether an array is subset of another array
+public class subsetofanotherarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a1[] = new int[n];
+        for (int i = 0; i < n; i++)
+            a1[i] = sc.nextInt();
+        int a2[] = new int[m];
+        for (int i = 0; i < m; i++)
+            a2[i] = sc.nextInt();
+        HashSet<Integer> hset = new HashSet<>();
+        for(int i=0;i<n;i++)
+        {
+            if(!hset.contains(a1[i]))
+             hset.add(a1[i]);
+        }
+        for(int i=0;i<m;i++)
+        {
+            if(!hset.contains(a2[i]))
+             return false;
+        }
+        return true;
+    }
+}
