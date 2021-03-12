fb668fd04d89c3fa32cecfbf6db67af6c2a9b2aa
Shashwat Sharma <shashwat2910@gmail.com>
2021-03-12 11:40:49 +0530

    Rain water Trapping

---
+++ Arrays/rainwatertrapping.java
@@ -0,0 +1,32 @@
+package Arrays;
+// Rain water Trapping Question done
+import java.util.Scanner;
+import java.util.Stack;
+
+public class rainwatertrapping {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int height[] = new int[n];
+        for (int i = 0; i < n; i++)
+            height[i] = sc.nextInt();
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<n;i++)
+        {
+            while(!stack.isEmpty() && (height[stack.peek()]<height[i]))
+            {
+                int pop_height = height[stack.peek()];
+                stack.pop();
+                if(stack.isEmpty())
+                 break;
+                int distance = i - stack.peek() - 1;
+                int min_distance = Math.min(height[stack.peek()], height[i]) - pop_height;
+                ans += distance * min_distance;
+            }
+            stack.push(i);
+        }
+        sc.close();
+        System.out.println(ans);
+    }
+}
